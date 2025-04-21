27733ced033f1ab8f23fec76b61d54f88ccb8876
chaiyaphat supharak <chaiyaphat2024@gmail.com>
2025-04-21 15:32:01 +0700

    feat: add routes

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"os"
+
+	"example.com/gin-backend-api/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	router := SetupRouter()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	router.Run(":" + os.Getenv("GO_PORT"))
+}
+
+func SetupRouter() *gin.Engine {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"API VERSION": "1.0.0",
-		})
-	})
-	router.Run(":3001") // listen and serve on 0.0.0.0:8080
+	apiV1 := router.Group("/api/v1") //localhost:3000/api/v1/...
+	routes.InitHomeRoutes(apiV1)
+	routes.InitUserRoutes(apiV1)
+	return router
 }
