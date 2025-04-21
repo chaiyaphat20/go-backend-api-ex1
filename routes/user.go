338af7214a79d39feb137f4ec08b9eb19c891532
chaiyaphat supharak <chaiyaphat2024@gmail.com>
2025-04-21 16:20:42 +0700

    feat: add controller

--- routes/user.go
+++ routes/user.go
@@ -1,13 +1,16 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	usercontroller "example.com/gin-backend-api/controllers/user"
+	"github.com/gin-gonic/gin"
+)
 
 func InitUserRoutes(rg *gin.RouterGroup) {
-	routerGroup := rg.Group("/users") //localhost/api/v1/users/
+	routerGroup := rg.Group("/users") //http://localhost:3001/api/v1/users
 
-	routerGroup.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"data": "users",
-		})
-	})
+	//http://localhost:3001/api/v1/users
+	routerGroup.GET("/", usercontroller.GetAll)
+
+	//http://localhost:3001/api/v1/users/register
+	routerGroup.POST("/register", usercontroller.Register)
 }
